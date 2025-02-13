0723652b6ede4222667792f96c263d0bc5dd49b9
Rashel124 <webcoderitrashel124@gmail.com>
2025-02-13 08:35:58 -0800

    Js Array

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+let twoDimensionalArray = [
+    [4, 31, 40, 23],
+    [10, 20, 56, 54],
+    [200, 100, 78, 90]
+];
+
+
+
+console.log(twoDimensionalArray[0][0]);
+console.log(twoDimensionalArray[0][1]);
+console.log(twoDimensionalArray[0][2]);
+console.log(twoDimensionalArray[0][3]);
+
+console.log(twoDimensionalArray[1][0]);
+console.log(twoDimensionalArray[1][1]);
+console.log(twoDimensionalArray[1][2]);
+console.log(twoDimensionalArray[1][3]);
+
+console.log(twoDimensionalArray[2][0]);
+console.log(twoDimensionalArray[2][1]);
+console.log(twoDimensionalArray[2][2]);
+console.log(twoDimensionalArray[2][3]);
